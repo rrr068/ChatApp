d725fe4df198f670d2e0e2cd3ba017fa75c42e4a
山田燎弥 <ryoya068@yamadaniwabiwatarunoMacBook-Air.local>
2024-05-19 14:02:25 +0900

    Footer追加

--- ChatApp_FrontEnd/src/components/Footer.tsx
+++ ChatApp_FrontEnd/src/components/Footer.tsx
@@ -1,8 +1,11 @@
 
 const Footer = () => {
   return (
-    <>
-    </>
+    <footer className="bg-gray-800 text-white py-4 fixed bottom-0 w-full">
+      <div className="container mx-auto text-center">
+        <p>&copy; Copyright ChatApp</p>
+      </div>
+    </footer>
   );
 }
 
